824624fb0656a7ba22199fc9563c58fccd4968df
msalah-hub <csagokum@mail.ru>
2026-04-09 22:16:18 +0300

    Fix mistakes

--- homework-8.js
+++ homework-8.js
@@ -1,47 +1,54 @@
 import { products } from './products.js';
 
 
-const productTemplate = document.querySelector('#product-template')
-const productList = document.querySelector('#product-list')
-
-
-function renderCards(count) {
-  const cards = document.querySelectorAll('.product-card');
-  cards.forEach((card, index) => {
-    if (index < count) {
-      card.style.display = 'block';
-    } else {
-      card.style.display = 'none';
-    }
-  });
-}
+const productTemplate = document.querySelector('#product-template');
+const productList = document.querySelector('#product-list');
 
 
 
 // 4 задание 
 
-const result = products.reduce((acc, product) => {
-  acc[product.name] = product.description
-  return acc;
-}, {})
 
-console.log(result);
+const cardDescription = products.reduce((acc, item) => {
+  acc.push({ [item.name]: item.description })
+  return acc;
+}, []);
 
 
 // 5 задание 
 
 
 function getCount() {
-  const count = Number(prompt("Сколько карточек отобразить? От 1 до 5"));
-  if (count >= 1 && count <= 5)
-    return count
-  else {
-    alert('Вы ввели неверное число');
-    return getCount();
-  }
+  const count = prompt("Сколько карточек отобразить? От 1 до 5");
+
+  if (count < 1 || count > 5 || isNaN(count)) {
+    alert("Введи число от 1 до 5!");
+    return 1;
+  };
+  return Number(count);
 };
 
+function renderCards(array) {
+  array.forEach((item) => {
+    const productClone = productTemplate.content.cloneNode(true)
+    productClone.querySelector('.product-card-img').src = item.image
+    productClone.querySelector('.product-card__for-skin').textContent = item.category
+    productClone.querySelector('.product-card__tittle').textContent = item.name
+    productClone.querySelector('.product-card__descr').textContent = item.description
+    productClone.querySelector('.product-card__price').textContent = item.price + ' ₽'
+
+    const compList = productClone.querySelector('.product-card__composition')
+    item.composition.forEach(c => {
+      const li = document.createElement('li')
+      li.textContent = c
+      compList.appendChild(li)
+    })
+
+    productList.appendChild(productClone)
+  });
+};
 
 const count = getCount();
-renderCards(count);
+const slicedProducts = products.slice(0, count);
+renderCards(slicedProducts);
 
